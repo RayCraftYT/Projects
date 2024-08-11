53775c1a4a4044fa23a9c5ee0fb8b38554e513c0
RayCraftYT <156703914+RayCraftYT@users.noreply.github.com>
2024-08-11 11:50:11 +0100

    ISBN generator now with attempt for 11 digit ISBN

--- ISBNgenerator/main.py
+++ ISBNgenerator/main.py
@@ -1,20 +1,34 @@
-numberArray = []
-calculationArray = []
-def entryCheck():
+def convertToISBN(number, length):
+    numberArray = []
+    newNumber = ""
     addResult = 0
-    tenDigitNumber = input("Please enter a ten digit number.")
-    x = tenDigitNumber.isnumeric()
-    if len(tenDigitNumber) == 10 and x == True:
-        for i in range(0,10):
-            numberArray.append(tenDigitNumber[i])
-            addResult = addResult + (int(tenDigitNumber[i]) * (11 - i)) 
-        modResult = addResult % 11 #Get remainer 
-        checkDigit = 11 - modResult
-        print(checkDigit)
-        numberArray.append(checkDigit)
-        print(numberArray)
+    checkDigit = ""
+    x = number.isnumeric()
+    if len(number) == length and x == True:
+        for i in range(0,length):
+            numberArray.append(number[i])
+            addResult = addResult + (int(number[i]) * (11 - i))
+        modResult = addResult % 11 #Get remainer
+        if modResult == 0:
+            checkDigit = "0"
+        elif modResult == 1:
+            checkDigit = "X"
+        else:
+            checkDigit = str(11 - modResult)
+            numberArray.append(checkDigit)
+        print("Result: " + str(addResult))
+        print("Check Digit: " + str(checkDigit))
+        print("Number Array: " + str(numberArray))
+        for i in range(0, len(numberArray)):
+            newNumber = newNumber + str(numberArray[i])
+        print("ISBN: " + newNumber)
+        return(newNumber)
     else:
         print("Sorry, that is not a valid digit.")
 
 print("Welcome to the ISBN generator, please enter a 10 digit number without any other characters and of the correct length to generate the corresponding ISBN!")
-entryCheck()
+tenDigitNumber = input("Please enter a ten digit number.")
+number = entryCheck(tenDigitNumber, 10)
+
+print ("ISBN:" + number)
+newNumber = convertToISBN(number, 11)
